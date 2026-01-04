6ae5cfa3578ef7f180a92f4433c4a32b5e276023
Maksim Belov <ultramaksim@gmail.com>
2026-01-04 23:36:08 +0300

    Feat[sig_check_util]: add docs

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/SignatureCheckUtil.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/SignatureCheckUtil.java
@@ -23,6 +23,14 @@ public class SignatureCheckUtil {
         this.mPublicKey = mPublicKey;
     }
 
+    /**
+     * Decode a bundle of signatures. A bundle of signatures has the following format:
+     * fileName1:base64-rsa4096-signature
+     * fileName2:base64-rsa4096-signature
+     * Invalid signatures aren't included in the resulting Map.
+     * @param bundle the original string of the bundle
+     * @return each decoded signature mapped to each file name
+     */
     public static Map<String,byte[]> decodeSignatureBundle(String bundle) {
         String[] signatureLines = bundle.split("\n");
         ArrayMap<String, byte[]> signatures = new ArrayMap<>(signatureLines.length);
@@ -38,12 +46,24 @@ public class SignatureCheckUtil {
         return signatures;
     }
 
+    /**
+     * Decode an RSA4096-encrypted signature from a Base64 string
+     * @param base64 the original base64 data
+     * @return the decoded bytes, or null if the data length isn't correct
+     */
     public static byte[] decodeRsa4096FromBase64(String base64) {
         byte[] rsaBytes = Base64.decode(base64, Base64.DEFAULT);
         if(rsaBytes.length != 512) return null;
         return rsaBytes;
     }
 
+    /**
+     * Verifies the signature of an input stream against the cert.pem certificate from app assets
+     * @param inputStream the original file stream
+     * @param signatureBytes the bytes of the encrypted signature
+     * @return whether the file signature check passed or not
+     * @throws IOException if there was an error while reading the file
+     */
     public boolean verify(InputStream inputStream, byte[] signatureBytes) throws IOException {
         byte[] ingestionBuffer = new byte[65535];
         try {
@@ -58,6 +78,13 @@ public class SignatureCheckUtil {
         }
     }
 
+    /**
+     * Reads in the cert.pem certificate from application assets and creates a SignatureCheckUtil
+     * to verify data against this certificate
+     * @param assetManager the AssetManager used to read the cert.pem file
+     * @return the SignatureCheckUtil instance
+     * @throws IOException if reading fails
+     */
     public static SignatureCheckUtil create(AssetManager assetManager) throws IOException {
         try (InputStream certificateStream = assetManager.open("cert.pem")) {
             CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
